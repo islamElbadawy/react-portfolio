4b38a0282250a4c8319c42a16dd39d34472f081f
islamElbadawy <islam.201211@gmail.com>
2024-01-05 18:38:27 +0200

    Finish project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {
+	Html,
+	Head,
+	Main,
+	NextScript,
+} from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<title>Islam Elbadawy Portfolio</title>
+				<link
+					rel="shortcut icon"
+					href="../public/favicon.ico"
+					type="image/x-icon"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
